59f92461db3881f1064403a3028477fc4423eddf
yanis59100 <meftouh.yanis@hotmail.com>
2025-02-23 07:21:56 +0100

    Add main.js - Vue.js app initialization with global styles

---
+++ apimef/frontend/src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+
+import './css/styles.css';
+import './css/images.css';
+import './css/inscription.css';
+import './css/connexion.css';
+import './css/contact.css';
+import './css/boutique.css';
+
+createApp(App).mount('#app');
